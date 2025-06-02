17d442f3f3bb1c7bd9a81ec73a9f14f78921e35f
Gino Naumann <g@gnaumann.de>
2025-06-02 20:01:50 +0200

    chore: add trainings

---
+++ lib/trainings.ts
@@ -0,0 +1,154 @@
+import {useTranslations} from 'next-intl'
+
+type TrainingType = 'participant' | 'speaker'
+
+export type Training = {
+  id: string
+  name: string
+  description: string
+  type: TrainingType
+  date: Date
+  days: number
+  technologies: string[]
+}
+
+type TrainingProps = {
+  t: ReturnType<typeof useTranslations>
+  technology?: string
+  type?: TrainingType
+}
+
+// const t = useTranslations('portfolio.trainings')
+
+export const getTrainings = ({
+  t,
+  technology,
+  type
+}: TrainingProps): Training[] => {
+  const trainings: Training[] = [
+    {
+      id: 'linux-debian-2016',
+      name: t('linux.debian.name'),
+      description: t('linux.debian.description'),
+      type: 'speaker',
+      date: new Date('2016-02-01'),
+      days: 10,
+      technologies: ['linux']
+    },
+    {
+      id: 'linux-debian-2017',
+      name: t('linux.debian.name'),
+      description: t('linux.debian.description'),
+      type: 'speaker',
+      date: new Date('2017-03-01'),
+      days: 10,
+      technologies: ['linux']
+    },
+    {
+      id: 'linux-centos-2017-2',
+      name: t('linux.centos.name'),
+      description: t('linux.centos.description'),
+      type: 'speaker',
+      date: new Date('2017-06-01'),
+      days: 5,
+      technologies: ['linux']
+    },
+    {
+      id: 'linux-debian-2018',
+      name: t('linux.debian.name'),
+      description: t('linux.debian.description'),
+      type: 'speaker',
+      date: new Date('2017-03-01'),
+      days: 10,
+      technologies: ['linux']
+    },
+    {
+      id: 'linux-debian-2019',
+      name: t('linux.debian.name'),
+      description: t('linux.debian.description'),
+      type: 'speaker',
+      date: new Date('2019-04-01'),
+      days: 10,
+      technologies: ['linux']
+    },
+    {
+      id: 'linux-debian-2020',
+      name: t('linux.debian.name'),
+      description: t('linux.debian.description'),
+      type: 'speaker',
+      date: new Date('2020-03-01'),
+      days: 10,
+      technologies: ['linux']
+    },
+    {
+      id: 'gitlab-2024-1',
+      name: t('gitlab.name'),
+      description: t('gitlab.description'),
+      type: 'speaker',
+      date: new Date('2024-02-01'),
+      days: 2,
+      technologies: ['gitlab']
+    },
+    {
+      id: 'gitlab-2024-2',
+      name: t('gitlab.name'),
+      description: t('gitlab.description'),
+      type: 'speaker',
+      date: new Date('2024-05-1'),
+      days: 2,
+      technologies: ['gitlab']
+    },
+    {
+      id: 'gitlab-2024-3',
+      name: t('gitlab.name'),
+      description: t('gitlab.description'),
+      type: 'speaker',
+      date: new Date('2024-08-1'),
+      days: 2,
+      technologies: ['gitlab']
+    },
+    {
+      id: 'gitlab-2024-4',
+      name: t('gitlab.name'),
+      description: t('gitlab.description'),
+      type: 'speaker',
+      date: new Date('2024-09-01'),
+      days: 2,
+      technologies: ['gitlab']
+    },
+    {
+      id: 'git-2023',
+      name: t('git.name'),
+      description: t('git.description'),
+      type: 'speaker',
+      date: new Date('2023-06-01'),
+      days: 2,
+      technologies: ['git']
+    },
+    {
+      id: 'git-2024',
+      name: t('git.name'),
+      description: t('git.description'),
+      type: 'speaker',
+      date: new Date('2024-06-01'),
+      days: 2,
+      technologies: ['git']
+    },
+    {
+      id: 'playwright-2024',
+      name: t('playwright.name'),
+      description: t('playwright.description'),
+      type: 'participant',
+      date: new Date('2024-11-01'),
+      days: 2,
+      technologies: ['playwright']
+    }
+  ]
+
+  return trainings.filter(training => {
+    const matchesTechnology =
+      !technology || training.technologies.includes(technology)
+    const matchesType = !type || training.type === type
+    return matchesTechnology && matchesType
+  })
+}
