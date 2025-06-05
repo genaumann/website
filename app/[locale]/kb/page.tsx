8bd2e4b1380a3acfb7dab533d2bb5b69d6c1075b
Gino Naumann <g@gnaumann.de>
2025-06-05 16:52:11 +0200

    chore(kb): add description

--- app/[locale]/kb/page.tsx
+++ app/[locale]/kb/page.tsx
@@ -34,16 +34,19 @@ export default async function Page({
 }) {
   const {locale} = await params
   const articles = await getArticlesByLocale(locale)
-  const t = await getTranslations()
+  const t = await getTranslations('kb')
 
   if (articles.length === 0) return null
 
   return (
     <div className="container flex flex-col md:flex-row md:justify-start md:gap-x-5 h-full">
       <div className="md:basis-1/3 md:relative sticky top-[97px] md:top-0">
-        <h1 className="text-5xl text-center md:text-start font-semibold md:sticky md:top-1/2 bg-background/75 md:bg-none backdrop-blur md:backdrop-blur-none py-4 md:py-0 w-full md:w-fit md:transform md:-translate-y-1/2">
-          {t('kb.title.long')}
-        </h1>
+        <div className="text-center md:text-start md:sticky md:top-1/2 bg-background/75 md:bg-none backdrop-blur md:backdrop-blur-none py-4 md:py-0 w-full md:w-fit md:transform md:-translate-y-1/2">
+          <h1 className="text-5xl font-semibold ">{t('title.long')}</h1>
+          <p className="text-muted-foreground max-w-9/12 md:max-w-none mx-auto md:mx-0 text-sm mt-2">
+            {t('description')}
+          </p>
+        </div>
       </div>
       <div className="mx-auto md:basis-2/3 md:flex md:justify-center md:border-l md:border-muted md:border-dashed">
         <ArticleOverview articles={articles} />
