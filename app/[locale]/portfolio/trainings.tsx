a53b4832a4dbee2c8fc9b8f2a7444875267f522a
Gino Naumann <g@gnaumann.de>
2025-06-10 22:29:34 +0200

    chore: style fixes in training and contact

--- app/[locale]/portfolio/trainings.tsx
+++ app/[locale]/portfolio/trainings.tsx
@@ -4,13 +4,15 @@ import {getTranslations} from 'next-intl/server'
 export default async function PortfolioTrainingsPage() {
   const t = await getTranslations('portfolio.trainings')
   return (
-    <section className="py-12 md:container bg-muted/20" id="trainings">
-      <div className="flex flex-col lg:flex-row lg:justify-between">
-        <h2 className="text-4xl font-bold mb-12 text-center lg:self-center">
-          {t('title')}
-        </h2>
-        <div className="max-w-full mx-auto lg:mx-0 block md:max-w-8/12">
-          <PortfolioTrainingSwiper />
+    <section className="py-12 bg-muted/20" id="trainings">
+      <div className="md:container">
+        <div className="flex flex-col lg:flex-row lg:justify-between">
+          <h2 className="text-4xl font-bold mb-12 text-center lg:self-center">
+            {t('title')}
+          </h2>
+          <div className="max-w-full mx-auto lg:mx-0 block md:max-w-8/12">
+            <PortfolioTrainingSwiper />
+          </div>
         </div>
       </div>
     </section>
--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -12,55 +12,55 @@ export default async function ContactSection({
 }) {
   const t = await getTranslations('contact.section')
   return (
-    <section
-      className={cn(
-        'container flex flex-col justify-center items-center gap-5 py-20',
-        className
-      )}>
-      <div className="flex flex-col">
-        <span>{t('subtitle')}</span>
-        <span className="text-3xl font-semibold text-center">{t('title')}</span>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full max-w-[800px]">
-        <Link
-          href={`mailto:${CONTACT.email}`}
-          className="flex flex-row md:flex-col gap-4 justify-between items-center bg-muted/20 p-4 rounded-lg shadow-all-md shadow-secondary space-y-4 w-full order-2 md:order-1">
-          <div className="text-xl mb-0">
-            <Icon name="envelope" />
-            <span className="ml-2">{t('email')}</span>
-          </div>
-          <span className="underline md:hidden mb-0">{CONTACT.email}</span>
-          <Button className="hidden md:block" variant="outline">
-            {CONTACT.email}
-          </Button>
-        </Link>
-        <Link
-          href="/contact"
-          className="flex flex-row md:flex-col gap-4 justify-between items-center bg-muted/20 p-4 rounded-lg space-y-4 order-1 md:order-2 shadow-all-md shadow-primary">
-          <div className="text-xl mb-0">
-            <Icon name="message" />
-            <span className="ml-2">{t('contactForm')}</span>
-          </div>
-          <div className="md:hidden mb-0">
-            <Icon name="arrow-right" size="xl" />
-          </div>
-          <Button className="hidden md:block" variant="outline">
-            {t('contactButton')}
-            <Icon className="md:hidden" name="arrow-right" />
-          </Button>
-        </Link>
-        <Link
-          href={`tel:${CONTACT.phone}`}
-          className="flex flex-row md:flex-col gap-4 justify-between items-center bg-muted/20 p-4 shadow-all-md shadow-secondary rounded-lg space-y-4 order-3">
-          <div className="text-xl mb-0">
-            <Icon name="phone" />
-            <span className="ml-2">{t('phone')}</span>
-          </div>
-          <span className="underline md:hidden mb-0">{CONTACT.phone}</span>
-          <Button className="hidden md:block" variant="outline">
-            {CONTACT.phone}
-          </Button>
-        </Link>
+    <section className={cn('py-20', className)}>
+      <div className="container flex flex-col justify-center items-center gap-5">
+        <div className="flex flex-col">
+          <span>{t('subtitle')}</span>
+          <span className="text-3xl font-semibold text-center">
+            {t('title')}
+          </span>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full max-w-[800px]">
+          <Link
+            href={`mailto:${CONTACT.email}`}
+            className="flex flex-row md:flex-col gap-4 justify-between items-center bg-muted/20 p-4 rounded-lg shadow-all-md shadow-secondary space-y-4 w-full order-2 md:order-1">
+            <div className="text-xl mb-0">
+              <Icon name="envelope" />
+              <span className="ml-2">{t('email')}</span>
+            </div>
+            <span className="underline md:hidden mb-0">{CONTACT.email}</span>
+            <Button className="hidden md:block" variant="outline">
+              {CONTACT.email}
+            </Button>
+          </Link>
+          <Link
+            href="/contact"
+            className="flex flex-row md:flex-col gap-4 justify-between items-center bg-muted/20 p-4 rounded-lg space-y-4 order-1 md:order-2 shadow-all-md shadow-primary">
+            <div className="text-xl mb-0">
+              <Icon name="message" />
+              <span className="ml-2">{t('contactForm')}</span>
+            </div>
+            <div className="md:hidden mb-0">
+              <Icon name="arrow-right" size="xl" />
+            </div>
+            <Button className="hidden md:block" variant="outline">
+              {t('contactButton')}
+              <Icon className="md:hidden" name="arrow-right" />
+            </Button>
+          </Link>
+          <Link
+            href={`tel:${CONTACT.phone}`}
+            className="flex flex-row md:flex-col gap-4 justify-between items-center bg-muted/20 p-4 shadow-all-md shadow-secondary rounded-lg space-y-4 order-3">
+            <div className="text-xl mb-0">
+              <Icon name="phone" />
+              <span className="ml-2">{t('phone')}</span>
+            </div>
+            <span className="underline md:hidden mb-0">{CONTACT.phone}</span>
+            <Button className="hidden md:block" variant="outline">
+              {CONTACT.phone}
+            </Button>
+          </Link>
+        </div>
       </div>
     </section>
   )
