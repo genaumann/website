8b68eb2a158c1ccbc8f658ddbaa56ec9cfe6d9ba
Gino Naumann <g@gnaumann.de>
2025-06-11 23:21:17 +0200

    ci: prod build within gh pipeline

--- lib/url.ts
+++ lib/url.ts
@@ -16,7 +16,7 @@ export const createQueryString = (
 
 export const origin =
   process.env.VERCEL_ENV === 'production'
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL || ''}`
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL || ''}`
     : process.env.VERCEL_ENV === 'preview'
-    ? `https://${process.env.VERCEL_BRANCH_URL}`
-    : 'http://localhost:3000'
+      ? `https://${process.env.VERCEL_BRANCH_URL}`
+      : 'http://localhost:3000'
