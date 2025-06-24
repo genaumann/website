e221369a58f890b17f1351b6750016d5352e4f09
Gino Naumann <g@gnaumann.de>
2025-06-25 00:08:45 +0200

    chore: add vercel flags setup

--- lib/string.ts
+++ lib/string.ts
@@ -1,4 +1,21 @@
+import {format} from 'date-fns'
+import {de} from 'date-fns/locale'
+
 export const sanitizeString = (string?: string) => {
   if (!string) return ''
   return string.replace(/[^a-zA-Z0-9]/g, '')
+}
+
+export const generateDateOptions = (): string[] => {
+  const dates: string[] = []
+  const start = new Date()
+  start.setDate(1)
+
+  for (let i = 0; i < 24; i++) {
+    const date = new Date(start.getFullYear(), start.getMonth() + i + 1, 0)
+    const formattedDate = format(date, 'MMMM yyyy', {locale: de})
+    dates.push(formattedDate)
+  }
+
+  return dates
 }
