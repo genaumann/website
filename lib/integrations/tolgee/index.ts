8b292be452c8fd3306dfc1903e519d4af721f427
Gino Naumann <g@gnaumann.de>
2025-07-01 22:10:12 +0200

    refactor(i18n): fetch from api

--- lib/integrations/tolgee/index.ts
+++ lib/integrations/tolgee/index.ts
@@ -1,16 +1,17 @@
 import {DevTools, Tolgee, FormatSimple, BackendFetch} from '@tolgee/web'
 import {FormatIcu} from '@tolgee/format-icu'
+import {origin} from '@/lib/url'
 
 const apiKey = process.env.NEXT_PUBLIC_TOLGEE_API_KEY
 const apiUrl = process.env.NEXT_PUBLIC_TOLGEE_API_URL
-const cdn = process.env.NEXT_PUBLIC_TOLGEE_CDN_URL
 
 export function TolgeeBase() {
   return Tolgee()
     .use(
       BackendFetch({
-        prefix: cdn,
-        next: {revalidate: 60, tags: ['i18n']}
+        prefix: 'api/i18n',
+        getPath: ({language, prefix, namespace}) =>
+          `${origin}/${prefix}/${namespace}/${language}`
       })
     )
     .use(FormatSimple())
