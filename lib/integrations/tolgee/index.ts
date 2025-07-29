e1e06dda72e3b0c1a025663e4f937f0400a7ecb1
Gino Naumann <g@gnaumann.de>
2025-07-29 22:16:25 +0200

    fix(tolgee): suppress translation error on prod

--- lib/integrations/tolgee/index.ts
+++ lib/integrations/tolgee/index.ts
@@ -1,6 +1,7 @@
 import {DevTools, Tolgee, FormatSimple} from '@tolgee/web'
 import {FormatIcu} from '@tolgee/format-icu'
 import {CreateFunctionBackend, fetchTolgee} from './plugin'
+import {LOCALES} from '@/locales'
 
 const apiKey = process.env.NEXT_PUBLIC_TOLGEE_API_KEY
 const apiUrl = process.env.NEXT_PUBLIC_TOLGEE_API_URL
@@ -15,10 +16,24 @@ export function TolgeeBase() {
       apiKey,
       apiUrl,
       onTranslationMissing: info => {
-        process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production' &&
-          console.error(
-            `Translation missing for ${info.ns}.${info.key} in ${info.language}`
+        if (process.env.NODE_ENV && process.env.NODE_ENV !== 'production') {
+          import(
+            `../../../public/i18n/${info.ns || 'common'}/${info.language || LOCALES.de}.json`
           )
+            .then(data => {
+              if (!data || !data[info.key]) {
+                console.error(
+                  `Translation missing for ${info.ns}.${info.key} in ${info.language || LOCALES.de}`
+                )
+              }
+            })
+            .catch(err => {
+              console.error(
+                `Error loading translation for ${info.ns}.${info.key} in ${info.language || LOCALES.de}`,
+                err
+              )
+            })
+        }
         return `${info.ns}.${info.key}`
       }
     })
