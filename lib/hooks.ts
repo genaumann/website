009abf12efa9e1165f62f644cf5f1c463fe22732
Gino Naumann <g@gnaumann.de>
2026-01-29 23:33:39 +0100

    feat add useColorScheme hook

---
+++ lib/hooks.ts
@@ -0,0 +1,20 @@
+'use client'
+
+import {useTheme} from 'next-themes'
+import {useMemo} from 'react'
+import { ColorScheme } from './types'
+
+type ThemeType = ColorScheme | 'system'
+
+export const useColorScheme = (): ColorScheme => {
+  const {theme: themeName, systemTheme} = useTheme() as {theme: ThemeType, systemTheme: ThemeType}
+
+  const theme = useMemo(() =>{
+    if (themeName !== 'system') {
+      return themeName
+    }
+    return systemTheme === 'dark' ? 'dark' : 'light'
+  }, [themeName, systemTheme])
+
+  return theme
+}
